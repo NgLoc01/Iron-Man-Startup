42636fbd2516044c65ebdb356d8bba15eaac58c2
ngloc01 <NgLoc01@gmail.com>
2026-07-12 16:50:38 +0200

    start listening automatically when app opens

--- Sources/IronManStartup/IronManStartup.swift
+++ Sources/IronManStartup/IronManStartup.swift
@@ -1,9 +1,19 @@
 import SwiftUI
 import AppKit
 
+let sharedDoubleClapDetector = DoubleClapDetectorService()
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+        sharedDoubleClapDetector.start()
+    }
+}
+
 @main
 struct IronManStartupApp: App {
-    @StateObject private var detector = DoubleClapDetectorService()
+    @StateObject private var detector = sharedDoubleClapDetector
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
 
     var body: some Scene {
         MenuBarExtra("Iron Man Startup", systemImage: "waveform.circle") {
